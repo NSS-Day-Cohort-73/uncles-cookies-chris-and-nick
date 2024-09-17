c0950b53fbeef5f42e37d8f5bd4e0f1501b2e9ec
Christopher Johnson <cjohnson7741@gmail.com>
2024-09-17 15:27:09 -0500

    created iceCream module

---
+++ scripts/iceCream.js
@@ -0,0 +1,23 @@
+import {database} from "./database.js"
+
+// set a HTML string variable then iterate through the database.iceCream array
+// for each element of the array create an HTML string for each property and then concatenate them together and return
+export const iceCreamList = () => {
+    
+    let iceCreamHTML = ""
+    for(const iceCream of database.iceCream){
+        iceCreamHTML += `
+        <div class="ice-cream-card">
+            <div>${iceCream.image}<div>
+
+            <h3>
+                 ${iceCream.name}
+            </h3>
+
+            <p>
+                ${iceCream.description}
+            </p>
+        </div>`
+    }
+    return iceCreamHTML
+    }
