5582e68a904360c51eae6f9d4523c45db4d4615d
herringvoices <nalherring@gmail.com>
2024-09-17 15:15:53 -0500

    Add generateCookies() to dynamically update cookies string.

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,1 +1,17 @@
+import {database} from "./database"
 
+export const generateCookies = () => {
+    cookieHTML = ''
+    
+    for (const item of database.cookies) {
+        cookieHTML += `
+        <div class="cookie-card">
+            <img src="${item.img}" alt="${item.name}">
+            <h3>${item.name}</h3>
+            <p>${item.description}</p>
+        </div>    
+        `
+    }
+    return cookieHTML
+
+}
