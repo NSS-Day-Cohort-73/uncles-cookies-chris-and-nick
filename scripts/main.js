688e4afda2b668771d8e07964fb337c3e1eb38f0
Christopher Johnson <cjohnson7741@gmail.com>
2024-09-18 10:47:06 -0500

    added main.js code

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+import { iceCreamList } from "./iceCream.js";
+import { generateCookies } from "./cookies.js"
+
+// initialize HTML strings
+let cookieHTML = generateCookies()
+let iceCreamHTML = iceCreamList()
+
+// select HTML elements
+let cookieDOM = document.querySelector(".cookie-box")
+let iceCreamDOM = document.querySelector(".ice-cream-container")
+
+// update them with the HTML strings
+cookieDOM.innerHTML = cookieHTML
+iceCreamDOM.innerHTML = iceCreamHTML
