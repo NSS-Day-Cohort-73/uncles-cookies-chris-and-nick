241b65e1363f71b101bca0bd494fa27686d29865
Christopher Johnson <cjohnson7741@gmail.com>
2024-09-18 13:44:53 -0500

    added alphabetical sorting to cookies and icecream

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,9 +1,12 @@
 import {database} from "./database.js"
 
 export const generateCookies = () => {
+    
+    let alphabeticalCookies = database.cookies.sort((elementOne,elementTwo) => elementOne.name.localeCompare(elementTwo.name))
+    
     let cookieHTML = ''
     
-    for (const item of database.cookies) {
+    for (const item of alphabeticalCookies) {
         cookieHTML += `
         <div class="cookie-card">
             <img src="${item.img}" alt="${item.name} Cookie">
