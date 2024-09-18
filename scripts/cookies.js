58ec460ac937e6f73bbd5d8f80bf458baf2d18e5
herringvoices <nalherring@gmail.com>
2024-09-18 11:19:25 -0500

    Finally fixing .js

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -6,7 +6,7 @@ export const generateCookies = () => {
     for (const item of database.cookies) {
         cookieHTML += `
         <div class="cookie-card">
-            <img src="${item.img}" alt="${item.name}">
+            <img src="${item.img}" alt="${item.name} Cookie">
             <h3>${item.name}</h3>
             <p>${item.description}</p>
         </div>    
